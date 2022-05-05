65481e92020733209460c4b509a82f1149c25d7c
NicerNewerCar <30351234+NicerNewerCar@users.noreply.github.com>
2022-05-05 15:36:18 -0400

    Added Auto playing

--- WordleSolver.py
+++ WordleSolver.py
@@ -1,6 +1,20 @@
-from enum import unique
-from nltk.corpus import brown, words
+from distutils.log import error
 import random
+from PIL import ImageGrab
+import pyautogui
+import time
+from pynput.keyboard import Key, Controller
+
+#FOR NYT DARK MODE WORDLE
+INVALID = (58, 58, 60)
+VALID = (83, 141, 78)
+INWORD = (181, 159, 59)
+
+class MousePos():
+    def __init__(self,x,y,bbox):
+        self.x = x
+        self.y = y
+        self.bbox = bbox
 
 class Positions:
     def __init__(self,nullLetters,correctLetter,avaliableLetters):
@@ -25,30 +39,26 @@ class Word:
         print("]")
         return ""
 
-
-def updateWord(word, guess):
-    if len(guess) == 5:
-        guess = guess.lower()
-        for i in range(5):
-            if word.word[i].correctLetter == "":
-                correctness = int(input("Is " + guess[i] + " correct? 1 for yes, 2 for in word, 3 for incorrect, 0 to exit: "))
-                if correctness == 1:
-                    word.word[i].correctLetter = guess[i]
-                    if guess[i] in word.unsetLetters:
-                        word.unsetLetters.remove(guess[i])
-                if correctness == 2:
-                    word.word[i].nullLetters.append(guess[i])
-                    word.word[i].avaliableLetters.remove(guess[i])
-                    if guess[i] not in word.unsetLetters:
-                        word.unsetLetters.append(guess[i])
-                if correctness == 3:
-                    for j in range(5):
-                        if guess[i] not in word.word[j].nullLetters:
-                            word.word[j].nullLetters.append(guess[i])
-                        if guess[i] in word.word[j].avaliableLetters:
-                            word.word[j].avaliableLetters.remove(guess[i])
-                if correctness == 0:
-                    exit()
+def updateWord(word, guess,cor):
+    guess = guess.lower()
+    for i in range(5):
+        if word.word[i].correctLetter == "":
+            correctness = cor[i]
+            if correctness == 1:
+                word.word[i].correctLetter = guess[i]
+                if guess[i] in word.unsetLetters:
+                    word.unsetLetters.remove(guess[i])
+            if correctness == 2:
+                word.word[i].nullLetters.append(guess[i])
+                word.word[i].avaliableLetters.remove(guess[i])
+                if guess[i] not in word.unsetLetters:
+                    word.unsetLetters.append(guess[i])
+            if correctness == 3:
+                for j in range(5):
+                    if guess[i] not in word.word[j].nullLetters:
+                        word.word[j].nullLetters.append(guess[i])
+                    if guess[i] in word.word[j].avaliableLetters:
+                        word.word[j].avaliableLetters.remove(guess[i])
 
 def getUniqueWords():
     f = open("words.txt", "r")
@@ -56,9 +66,6 @@ def getUniqueWords():
     for x in f:
         unique_words.append(x.strip())
     return unique_words
-
-
-
 
 def suggestWord(word, unique_words):
     if unique_words == []:
@@ -82,13 +89,77 @@ def suggestWord(word, unique_words):
             valid_words.append(possible_word)
     return valid_words
 
+def setMousePos():
+    mousePos = pyautogui.position()
+    x = mousePos[0]
+    y = mousePos[1]
+    return MousePos(x,y,(x,y,x+330,y+400))
 
+def initMouse():
+    input("Once you press enter you will have 5 seconds to align the mouse with the top left of the play aera to set the bounding box")
+    time.sleep(5)
+    mp = setMousePos()
+    return mp
+
+def evalRow(mp, row):
+    im =  ImageGrab.grab(bbox=mp.bbox)
+    width, height = im.size
+    cropH = int(height/6)
+    upperBound = cropH * row
+    lowerBound = upperBound + cropH
+    a = im.crop((0, upperBound, width, lowerBound))
+    width, height = a.size
+    cropW = int(width/5)
+    correctness = []
+    for i in range(5):
+        leftBound = cropW * i
+        rightBound = leftBound + cropW
+        b = a.crop((leftBound,0,rightBound,height))
+        letterWidth, letterHeight = b.size
+        evalX = int(letterWidth/2 + letterWidth/4)
+        evalY = int(letterHeight/2 + letterHeight/4)
+        b = b.convert('RGB')
+        rgb = b.getpixel((evalX, evalY))
+        if rgb == VALID:
+            correctness.append(1)
+        elif rgb == INWORD:
+            correctness.append(2)
+        elif rgb == INVALID:
+            correctness.append(3)
+        else:
+            error("INVALID RGB VALUE IN ROW " + str(row) + " LETTER " + str(i))
+    return correctness
+
+def type(keyboard,word):
+    for char in word:
+        keyboard.press(char)
+        keyboard.release(char)
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+
+# set up variables and get the bbox pos
 alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+keyboard = Controller()
+starters = ['slice','tried','crane','reach','bakes','roast','adieu']
+random.shuffle(starters)
 word = Word(alphabet)
-updateWord(word,input("Enter a guess: "))
+mp = initMouse()
+
+#make init guess
+input("Once you press enter you will have 3 seconds to select the game window before the bot plays")
+time.sleep(3)
+type(keyboard,starters[0])
+time.sleep(2.5)
+cor = evalRow(mp,0)
+updateWord(word,starters[0],cor)
 suggestions = suggestWord(word,[])
-for _ in range(0,5):
+
+for i in range(1,6):
     random.shuffle(suggestions)
-    print(suggestions[0])
-    updateWord(word,suggestions[0])
+    type(keyboard,suggestions[0])
+    time.sleep(2.5)
+    cor = evalRow(mp,i)
+    if (all(x == 1 for x in cor)):
+        exit()
+    updateWord(word,suggestions[0],cor)
     suggestions = suggestWord(word,suggestions)
